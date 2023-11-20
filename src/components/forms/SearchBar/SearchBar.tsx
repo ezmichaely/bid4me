1c0a842776a82d47c3f0e723a62466ca63e57df4
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-20 16:20:35 +0800

    remove comments

--- src/components/forms/SearchBar/SearchBar.tsx
+++ src/components/forms/SearchBar/SearchBar.tsx
@@ -1,21 +1,24 @@
 "use client"
 
-import { Input } from "@nextui-org/react";
+// import { Input } from "@nextui-org/react";
 import { Search } from 'lucide-react';
 
 
 export default function SearchBar() {
   return (
-    <Input
-      type="search"
-      placeholder="search in bid4me"
-      labelPlacement="outside"
-      startContent={
-        <div className="rotate-90">
-          <Search size={20} color="#0F172A" strokeWidth={3} />
-        </div>
-      }
-      classNames="rounded-full"
-    />
+    <div>
+
+    </div>
+    // <Input
+    //   type="search"
+    //   placeholder="search in bid4me"
+    //   labelPlacement="outside"
+    //   startContent={
+    //     <div className="rotate-90">
+    //       <Search size={20} color="#0F172A" strokeWidth={3} />
+    //     </div>
+    //   }
+    //   classNames="rounded-full"
+    // />
   )
 }
