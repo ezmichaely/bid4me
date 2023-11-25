32f984156ecf4a5a3c3dc6262c19a36a6d2cad16
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-25 18:02:35 +0800

    shad/cn ui patch

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,20 +1,3 @@
-<<<<<<< Updated upstream
-import Providers from './common/Providers';
-import BrandName from './common/brand/BrandName';
-import Header from './common/Header';
-import Footer from './common/Footer';
-import HeaderLogo from './common/brand/HeaderLogo';
-import HeaderAvatar from './common/ProfileAvatar';
-import AsideNav from './common/AsideNav';
-import MobileNav from './common/MobileNav';
-import NotificationBell from './common/NotificationBell';
-
-import HeroSwiper from './common/carousel/HeroSwiper';
-
-import SearchBar from './forms/SearchBar';
-import LoginForm from './forms/LoginForm';
-import SignupForm from './forms/SignupForm';
-=======
 import NextAuthProvider from './providers/NextAuthProvider';
 import ThemeSwitcher from './common/ThemeSwitcher';
 
@@ -39,18 +22,11 @@ import HeroSwiper from './carousel/HeroSwiper';
 import SearchBar from './forms/SearchBar';
 import LoginForm from './forms/LoginForm';
 // import SignupForm from './forms/SignupForm';
->>>>>>> Stashed changes
 
 
 
 
 export {
-<<<<<<< Updated upstream
-  Providers, BrandName,
-  Header, Footer,
-  HeaderLogo, HeaderAvatar,
-  AsideNav, MobileNav,
-=======
   NextAuthProvider,
   ThemeSwitcher,
   MainNav, TopNav, MobileNav,
@@ -58,7 +34,6 @@ export {
   Header, Footer,
   AvatarProfile,
 
->>>>>>> Stashed changes
   NotificationBell,
 
   HeroSwiper,
