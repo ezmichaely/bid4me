4ad4616ad4ecb4fefeb997cbf533300916540901
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-25 18:13:21 +0800

    pages duh??

---
+++ src/app/(pages)/product/layout.tsx
@@ -0,0 +1,38 @@
+import { Inter, Kanit } from 'next/font/google'
+import { inter, kanit } from '@/assets/fonts';
+import {
+  NextAuthProvider,
+  Header
+} from "@/components";
+import { NextThemeProvider } from '@/components/providers/NextThemeProvider';
+import '@/styles/globals.css'
+
+const interOnline = Inter({ subsets: ['latin'] })
+const kanitOnline = Kanit({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic']
+})
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const htmlClass = `light ${inter.variable} ${kanit.variable}`
+  // ${interOnline.className} ${kanitOnline.className}`
+
+
+  return (
+    <html lang="en" className={htmlClass} suppressHydrationWarning>
+      <body>
+        <NextThemeProvider
+          attribute='class'
+          defaultTheme='white'
+          enableSystem
+        >
+          <NextAuthProvider>
+            <Header />
+            {children}
+          </NextAuthProvider>
+        </NextThemeProvider>
+      </body>
+    </html>
+  )
+}
