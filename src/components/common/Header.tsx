d31b1db1a2a401063f97d9c9995b6174fd712276
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-25 18:01:16 +0800

    shad/cn ui patch

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,26 +1,6 @@
 "use client"
 
 import Link from 'next/link'
-<<<<<<< Updated upstream
-import { Divider } from "@nextui-org/react";
-import { signOut, useSession } from 'next-auth/react';
-import { usePathname } from 'next/navigation';
-import styles from '@/styles//header.module.css';
-import { colors } from '@/lib/constants';
-import {
-  HeaderLogo, SearchBar,
-  BrandName, HeaderAvatar,
-  MobileNav, NotificationBell
-} from '@/components'
-import {
-  ShoppingCart, Zap, Gavel,
-  Shell, DatabaseZap, Truck,
-} from 'lucide-react';
-
-
-export default function Header() {
-
-=======
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import styles from '@/styles//header.module.css';
@@ -35,114 +15,12 @@ import {
 
 
 export default function Header() {
->>>>>>> Stashed changes
   const path = usePathname();
   const session = useSession();
   const status = session.status;
 
   return (
     <header className={styles.header}>
-<<<<<<< Updated upstream
-      <div className={styles.topContainer}>
-        {/* LOGO */}
-        <div className={styles.logoPlaceholder}> <HeaderLogo /> </div>
-
-        {/* TOP BAR */}
-        <div className={styles.topBarContainer}>
-          <div className={styles.topBarContent}>
-            <Link href="/download"
-              className={`navLink ${path === '/download' ? 'active' : ''}`}>
-              DOWNLOAD APP
-            </Link>
-            <Link href="/becomeaseller"
-              className={`navLink ${path === '/becomeaseller' ? 'active' : ''}`}>
-              BECOME A SELLER
-            </Link>
-
-            <Divider orientation="vertical" className="w-[2px] bg-accent" />
-
-            {status === 'unauthenticated' && (
-              <>
-                <Link href="/login"
-                  className={`navLink ${path === '/login' ? 'active' : ''}`}>
-                  LOGIN
-                </Link>
-                <Link href="/signup"
-                  className={`navLink ${path === '/signup' ? 'active' : ''}`}>
-                  SIGN UP
-                </Link>
-              </>
-            )}
-
-            {status === 'authenticated' && (
-              <>
-                <NotificationBell />
-                <button onClick={() => signOut()} className={styles.link}>LOGOUT</button>
-              </>
-            )}
-          </div>
-
-          <div className='relative w-full lg:hidden flex justify-end xs:justify-center py-2 px-6 gap-4 xs:gap-0'>
-            <BrandName size='small' />
-
-            {status === 'authenticated' && (
-              <>
-                <Divider orientation={'vertical'} className='block xs:hidden w-[2px] bg-accent' />
-                <div className='block xs:absolute right-1 xs:mr-4'>
-                  <NotificationBell />
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-
-        {/* MIDDLE WHITE BG */}
-        <div className={styles.whiteBg} />
-
-        {/* SEARCH BAR */}
-        <div className={styles.searchBarContainer}>
-          <div className={styles.searchBarContent}>
-            <SearchBar />
-
-            <Link href="/cart">
-              <ShoppingCart size={28} color={colors.secondary}
-                strokeWidth={2} className='hidden lg:block' />
-            </Link>
-
-            <MobileNav />
-
-            {status === 'authenticated' && (
-              <HeaderAvatar />
-            )}
-
-          </div>
-        </div>
-      </div>
-
-      {/* BOT BAR */}
-      <div className={styles.botContainer}>
-        <div className={styles.botContent}>
-          <Link href="/flashsales"
-            className={`navLink ${path === '/flashsales' ? 'active' : ''}`}>
-            <Zap /> FLASH SALES
-          </Link>
-          <Link href="/raffle"
-            className={`navLink ${path === '/raffle' ? 'active' : ''}`}>
-            <Shell /> RAFFLE
-          </Link>
-          <Link href="/auction"
-            className={`navLink ${path === '/auction' ? 'active' : ''}`}>
-            <Gavel /> AUCTION
-          </Link>
-          <Link href="/freeshipping"
-            className={`navLink ${path === '/freeshipping' ? 'active' : ''}`}>
-            <Truck /> FREE SHIPPING
-          </Link>
-          <Link href="/installment"
-            className={`navLink ${path === '/installment' ? 'active' : ''}`}>
-            <DatabaseZap /> INSTALLMENT
-          </Link>
-=======
       <div className={styles.topBarContainer}>
         <TopNav />
       </div>
@@ -174,13 +52,8 @@ export default function Header() {
       <div className={styles.botContainer}>
         <div className={styles.botContent}>
           <MainNav />
->>>>>>> Stashed changes
         </div>
       </div>
     </header>
   )
-<<<<<<< Updated upstream
 }
-=======
-}
->>>>>>> Stashed changes
