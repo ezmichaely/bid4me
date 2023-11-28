9008ab14f4b97b8127dcb63f51efeb2dd025fb17
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-28 19:21:17 +0800

    remove auth

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,43 +1,26 @@
-import { Inter, Kanit } from 'next/font/google'
-// import { inter, kanit } from '@/assets/fonts';
-import {
-  NextAuthProvider,
-  Header
-} from "@/components";
+import { inter, kanit } from '@/assets/fonts';
+import { Header } from "@/components";
 import { NextThemeProvider } from '@/components/providers/NextThemeProvider';
 import '@/styles/globals.css'
 
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
-  style: ['normal'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
-const kanit = Kanit({
-  subsets: ['latin'],
-  variable: '--font-kanit',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  style: ['normal', 'italic'],
-  display: 'swap',
-})
 
-export default function UserLayout({ children }: { children: React.ReactNode }) {
+
+export default function UserLayout({ children }:
+  { children: React.ReactNode }) {
   const htmlClass = `light ${inter.variable} ${kanit.variable}`
   // const htmlClass = `light ${interOnline.className} ${kanitOnline.className}`
 
   return (
-    <html lang="en" className={htmlClass} suppressHydrationWarning>
+    <html lang="en" className={htmlClass}
+      suppressHydrationWarning>
       <body>
         <NextThemeProvider
           attribute='class'
           defaultTheme='light'
           enableSystem={false}
         >
-          <NextAuthProvider>
-            <Header />
-            {children}
-          </NextAuthProvider>
+          <Header />
+          {children}
         </NextThemeProvider>
       </body>
     </html>
