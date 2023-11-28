5324130f4a325c60111ab2c6e78ec27915d0f8f5
EZ MICHAEL YUCOR <76212358+ezmichaely@users.noreply.github.com>
2023-11-28 14:49:13 +0800

    clean up

--- src/app/user/message/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MessagePage() {
-  return (
-    <div>
-
-    </div>
-  )
-}
--- src/components/sections/MainHero.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function MainHero() {
-  return (
-    <div>
-
-    </div>
-  )
-}
